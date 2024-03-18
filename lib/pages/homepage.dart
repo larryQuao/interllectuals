c9104d62c2462f9c49878cde0eed90f1f64c7cf9
larryq561 <131362406+larryQuao@users.noreply.github.com>
2024-03-18 20:09:06 +0000

    Changes made

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,43 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:interllectuals/widgets/navigation.dart';
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({super.key});
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  int _selectedIndex = 0;
-
-  void _onItemSelected(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  final List<NavigationItem> _item = [
-    NavigationItem('Home'),
-    NavigationItem('About'),
-    NavigationItem('Services'),
-    NavigationItem('Advisor Blog'),
-  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Interllectuals'),
-        actions: [
-          TextButton(
-            onPressed: () {},
-            child: Text('Login'),
-          ),
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Container(
+                  child: Image.asset(''),
+                )
+              ],
+            )
+          ],
+        ),
       ),
-      body: const SingleChildScrollView(),
-      backgroundColor: Color.fromARGB(255, 106, 149, 222),
     );
   }
 }
